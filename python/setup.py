ac7c3c1d4ccb28d9febaa16bc607f1cd4f43153e
Zamin Iqbal <zi@ebi.ac.uk>
2019-05-15 16:54:32 +0000

    Bumop version to 0.7.3

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='clockwork',
-    version='0.7.2',
+    version='0.7.3',
     description='Pipeline code for CRyPTIC project',
     packages = find_packages(),
     author='Martin Hunt',
