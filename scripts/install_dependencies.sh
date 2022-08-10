9be0189aceaf6ddfb38dd1a7853c0e5e9cb72f30
Martin Hunt <martin.g.hunt@gmail.com>
2022-08-10 16:33:09 +0100

    update cluster_vcf_records 0.13.2; minos 0.12.4

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -262,5 +262,5 @@ git checkout 5819787614a263a9f35fd0c247442f092ab174ff
 pip3 install .
 
 #________________________ minos _____________________________#
-pip3 install 'cluster_vcf_records==0.13.2'
-pip3 install git+https://github.com/iqbal-lab-org/minos@v0.12.0
+pip3 install 'cluster_vcf_records==0.13.3'
+pip3 install git+https://github.com/iqbal-lab-org/minos@v0.12.4
