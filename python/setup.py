e4209b96a25d705ebbdbfda29dc3cf198ef81c3e
Martin Hunt <martin.g.hunt@gmail.com>
2022-08-10 17:42:56 +0100

    Version bump 0.11.2

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="clockwork",
-    version="0.11.1",
+    version="0.11.2",
     description="Pipeline code for CRyPTIC project",
     packages=find_packages(),
     author="Martin Hunt",
@@ -14,7 +14,7 @@ setup(
     test_suite="nose.collector",
     tests_require=["nose >= 1.3"],
     install_requires=[
-        "cluster_vcf_records >= 0.11.1",
+        "cluster_vcf_records >= 0.13.3",
         "python-dateutil >= 2.6.1",
         "openpyxl >= 2.4.7",
         "pyfastaq >= 3.14.0",
