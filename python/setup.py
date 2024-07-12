b5adc5ea635aca7ac60a03ee9aba78ea19d89d81
Martin Hunt <martinghunt@users.noreply.github.com>
2024-07-12 11:09:25 +0100

    Version bump 0.12.5

--- python/setup.py
+++ python/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="clockwork",
-    version="0.12.3",
+    version="0.12.5",
     description="Pipeline code for CRyPTIC project",
     packages=find_packages(),
     author="Martin Hunt",
